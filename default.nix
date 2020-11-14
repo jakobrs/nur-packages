fbce7cda6ea4a0c32236a12122bb02ef0b212655
jakobrs <jakobrs100@gmail.com>
2020-11-14 12:18:51 +0100

    wireplumber: init at 0.3.0

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
-{
+rec {
   n2n = pkgs.callPackage ./pkgs/n2n {};
   mcstatus = pkgs.python3Packages.callPackage ./pkgs/mcstatus {};
 
   cpptoml = pkgs.callPackage ./pkgs/cpptoml {};
+  wireplumber = pkgs.callPackage ./pkgs/wireplumber { inherit cpptoml; };
 }
