1d6e3096aec17ff8c264bece535d30ab268dc2c4
jakobrs <jakobrs100@gmail.com>
2021-08-07 14:03:05 +0200

    wireplumber: add introspection and docs

--- pkgs/wireplumber/default.nix
+++ pkgs/wireplumber/default.nix
@@ -1,5 +1,8 @@
 { stdenv, lib, fetchFromGitLab, fetchpatch
-, ninja, meson, pkg-config, cmake, git
+, ninja, meson, pkg-config, cmake, git, doxygen, gobject-introspection
+, python3
+, lxml # for gobject-introspection
+, sphinx, sphinx_rtd_theme, breathe # for docs
 , glib, pipewire, lua5_4, systemd }:
 
 stdenv.mkDerivation rec {
@@ -14,11 +17,14 @@ stdenv.mkDerivation rec {
     hash = "sha256:1j88pl8yfj3vcsh9y57a542msn1vk8qcwcy2qqgjg2y8lfg7qi6b";
   };
 
-  nativeBuildInputs = [ ninja meson pkg-config cmake git ];
+  nativeBuildInputs = [
+    ninja meson pkg-config cmake git doxygen gobject-introspection
+    python3 lxml sphinx sphinx_rtd_theme breathe
+  ];
 
   buildInputs = [ glib pipewire lua5_4 systemd ];
 
-  mesonFlags = [ "-Dintrospection=disabled" "-Ddoc=disabled" "-Dsystem-lua=true" ];
+  mesonFlags = [ "-Dsystem-lua=true" ];
 
   hardeningDisable = [ "format" ];
 
